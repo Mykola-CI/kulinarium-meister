e26a92bfb4c949a2ed05874315c2d46fa9cbea49
Mykola Kuievda <dankraig23@gmail.com>
2025-09-01 16:17:07 +0100

    Finalise section product categories

--- src/js/index.js
+++ src/js/index.js
@@ -37,20 +37,49 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 
-  // Listen for Bootstrap dropdown show event
-  document.querySelectorAll('.dropdown').forEach(function (dropdown) {
-    dropdown.addEventListener('shown.bs.dropdown', function () {
-      const menu = dropdown.querySelector('.Header-nav-folder')
-      if (menu) {
-        // Force override inline styles
-        menu.style.setProperty('inset', '10px auto auto 0px', 'important')
-        menu.style.setProperty('margin', '0px', 'important')
-        menu.style.setProperty(
-          'transform',
-          'translate(-5px, 13px)',
-          'important'
-        )
-      }
-    })
+  // Hero Kaleidoscope main class
+  class HeroKaleidoscope {
+    constructor(selector, options = {}) {
+      this.container = document.querySelector(selector)
+      this.images = this.container.querySelectorAll('img')
+      this.currentIndex = 0
+      this.interval = options.interval || 3000
+      this.timer = null
+      this.init()
+    }
+
+    init() {
+      this.start()
+      // Pause on hover
+      this.container.addEventListener('mouseenter', () => this.pause())
+      this.container.addEventListener('mouseleave', () => this.resume())
+    }
+
+    start() {
+      if (this.isRunning) return // ← Prevent multiple intervals
+
+      this.isRunning = true
+      this.timer = setInterval(() => this.next(), this.interval)
+    }
+
+    next() {
+      this.images[this.currentIndex].classList.remove('active')
+      this.currentIndex = (this.currentIndex + 1) % this.images.length
+      this.images[this.currentIndex].classList.add('active')
+    }
+
+    pause() {
+      if (this.timer) clearInterval(this.timer)
+      this.timer = null
+      this.isRunning = false
+    }
+
+    resume() {
+      if (!this.isRunning) this.start()
+    }
+  }
+
+  window.heroKaleidoscope = new HeroKaleidoscope('#heroKaleidoscope', {
+    interval: 2500, // 3 seconds between transitions
   })
 })
